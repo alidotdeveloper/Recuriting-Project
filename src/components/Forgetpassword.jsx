97355aa62aa92069f1049e9d592d2d4c9aac5269
Ali <alihassnain330@gmail.com>
2023-08-31 00:40:02 +0500

    front for the git done with some changes

--- src/components/Forgetpassword.jsx
+++ src/components/Forgetpassword.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
+
 
 function Forgetpassword() {
+    const [values, setValues] = useState({
+        email: '',
+      })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post('http://localhost:8080/api/forgetpassword', values)
+            .then(res => {
+                
+                if (res.data.status === "ok") {
+                    console.log(res.data.message)
+                return res.json(200)
+                }
+                else {
+                    console.log("email not send")
+                }
+                
+            }).catch(err=>console.log(err))
+    
+
+    }
     return(
     <div className="main">
         <div className="background">
             <div className="shape"></div>
             <div className="shape"></div>
         </div>
-        <form onSubmit="" method='POST' className='forget-form'>
-      
+            <form onSubmit={handleSubmit } method='POST' className='forget-form'>
             <h3>Forget password</h3>
             <label >Email</label>
-            <input type='text' id="email"  />
+            <input type='text' id="email" onChange={e=> setValues({...values, email:e.target.value})}/>
             <button class="login-btn">Send</button>
             <div className='danger-text'></div>
         </form>
