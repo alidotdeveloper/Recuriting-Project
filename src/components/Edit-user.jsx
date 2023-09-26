10b4b97dad352c574cf12a2bc49b311b22cbfbc5
Ali <alihassnain330@gmail.com>
2023-09-27 02:38:02 +0500

    user updation fixed

--- src/components/Edit-user.jsx
+++ src/components/Edit-user.jsx
@@ -1,38 +1,45 @@
-import React, {useState } from 'react';
+import React, {useState, useEffect } from 'react';
 import "../style/dashboard.css";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
 
 function EditUser() {
+ const{userId,username,email,password,role} = useParams();
 
   const [values, setValues] = useState({
-    email: '',
-    password: '',
-    role: '',
-    username:''
+    email: email || "",
+    password: password || "",
+    role: role || "",
+    username:username || ""
   })
   const [err, seterr] = useState(null)
     
-    
- 
-    
-  let {userId,username,email,password,role} = useParams();
-  console.log(userId);
   
-  const handleclick = () => {
+  const handleclick = (e) => {
+    e.preventDefault();
  
     axios.put(`http://localhost:8080/api/showuser/${userId}/${username}/${email}/${password}/${role}`, values)
       .then((res) => {
-        if ((res.data.status = "Sucess")) {
-          seterr(res.data)
+       console.log(res)
+        if ((res.statusText === "OK")) {
+          seterr(res.data.message)
         } else {
-          seterr("Response data is not in array")
+          seterr("Soory,User is not updated there is some issue")
         }
       })
       .catch((err) => {
         seterr("Got Error In catch: " + err.message)
       })
   }
+
+  useEffect(() => {
+    setValues({
+      email: email || '',
+      password: password || '',
+      role: role || '',
+      username: username || ''
+    });
+  }, [userId, username, email, password, role]);
   
   return (
     <div className="main">
@@ -44,11 +51,11 @@ function EditUser() {
         
         <h3>Update User</h3>
         <label >Username</label>
-        <input type='text' id='passowrd' value={username}  onChange={e=> setValues({...values, password:e.target.value})} />
+        <input type='text' id='username' value={values.username}   onChange={e=> setValues({...values, username:e.target.value})} />
         <label >Email</label>
-        <input type='text' id="email" value={email}  onChange={e=> setValues({...values, email:e.target.value})}  />
+        <input type='text' id="email" value={values.email}  onChange={e=> setValues({...values, email:e.target.value})}  />
         <label >Password</label>
-        <input type='password' id='passowrd' value={password} onChange={e => setValues({ ...values, password: e.target.value })} />
+        <input type='password' id='password' value={values.password} onChange={e => setValues({ ...values, password: e.target.value })} />
         <label> Select Role </label>
         <select>
         <option value="Admin"  onChange={e=> setValues({...values, admin:e.target.value})}>Admin</option>
